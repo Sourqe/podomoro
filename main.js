affbcd22e5f7c3594cf7f53688d2ee17aa38f4ec
Sourqe <nickvandewaterlaat@hotmail.com>
2018-08-13 16:24:09 +0200

    clean up

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 var startButton = document.getElementById("start");
 var timerRunning = false;
 var stopResetOn = false;
-var workNow = false;
+var workNow = true;
+var timeLeft = 0;
+var timerId;
+var startTime;
+var timeLeft;
+
+$('#pause').addClass("disabledButton");
+$('#stop').addClass("disabledButton");
+$('#pause').prop("disabled", true);
+$('#stop').prop("disabled", true);
 
 function prefixZero(time) {
     if (time < 10) {
@@ -11,19 +20,23 @@ function prefixZero(time) {
     }
 }
 
+function clearTimer() {
+    clearInterval(timerId);
+}
+
 function startTimer(workNow) {
-    if (workNow === true) {
-        var startTime = parseInt($("#workDisplay").text()) * 60;
+    if (workNow === true  && timeLeft === 0) {
+        startTime = parseInt($("#workDisplay").text()) * 60;
         $("#timeText").text($("#workDisplay").text() + ":00");
 
     } 
     
     if (workNow === false) {
-        var startTime = parseInt($("#breakDisplay").text()) * 60;
+        startTime = parseInt($("#breakDisplay").text()) * 60;
         $("#timeText").text($("#breakDisplay").text() + ":00");
     }
 
-    var time = setInterval(function() {
+    timerId = setInterval(function() {
         startTime--;
 
         var minutes = prefixZero(Math.floor(startTime / 60));
@@ -32,12 +45,25 @@ function startTimer(workNow) {
         $("#timeText").text(minutes + ":" + seconds);
 
         if (startTime == 0) {
-            clearInterval(time);
+            clearTimer();
 
             if (workNow === true) {
                 workNow = false;
+
+                $('#bottomText').removeClass("initialOpacity");
+                $('#bottomText').addClass("gray");
+
+                $('#topText').addClass("initialOpacity");
+                $('#topText').removeClass("gray");
+
             } else {
                 workNow = true;
+
+                $('#topText').removeClass("initialOpacity");
+                $('#topText').addClass("gray");
+
+                $('#bottomText').addClass("initialOpacity");
+                $('#bottomText').removeClass("gray");
             }
 
             startTimer(workNow);
@@ -48,6 +74,16 @@ function startTimer(workNow) {
 
 $(function() {
     $("#start").click(function() {
+
+        $('#start').prop("disabled", true);
+        $('#start').addClass("disabledButton");
+
+        $('#pause').removeClass("disabledButton");
+        $('#pause').prop("disabled", false);
+
+        $('#stop').removeClass("disabledButton");
+        $('#stop').prop("disabled", false);
+
         workNow = true;
         startTimer(workNow);
     }),
@@ -68,7 +104,7 @@ $(function() {
     $("#breakUp").click(function() {
         var currentTime = parseInt($("#breakDisplay").text());
         if (currentTime < 50) {
-            var workTime = currentTime - 1;
+            var workTime = currentTime + 1;
             $("#breakDisplay").text(workTime);
             if (workNow === false) {
                 $("#configureMinutes").text(workTime);
@@ -101,6 +137,45 @@ $(function() {
             }
         } else {
             alert("The duration of a break has to be larger than 0.");
+        }
+    }),
+
+    $("#pause").click(function() {
+        if (workNow === true) {
+            workNow = false;
+
+            $('#pause').prop("disabled", true);
+            $('#pause').addClass("disabledButton");
+
+            $('#start').removeClass("disabledButton");
+            $('#start').prop("disabled", false);
+            
+            $('#stop').addClass("disabledButton");
+            $('#stop').prop("disabled", true);
+            
+            // pause the timer
+            timeLeft = startTime;
+            clearTimer();
+        }
+    }),
+
+    $("#stop").click(function() {
+        if (workNow === true) {
+            workNow = false;
+
+            $('#pause').prop("disabled", true);
+            $('#pause').addClass("disabledButton");
+
+            $('#stop').prop("disabled", true);
+            $('#stop').addClass("disabledButton");
+
+            $('#start').prop("disabled", false);
+            $('#start').removeClass("disabledButton");
+            
+            timeLeft = 0;
+            clearTimer();
+
+            $("#timeText").text($("#workDisplay").text() + ":00");
         }
     })
 
